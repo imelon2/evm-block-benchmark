df7f4e415c86029517231f7dc3f15102784d6b3c
imelon2 <imelon279@naver.com>
2024-11-22 11:59:20 +0900

    feat nodemodule -> js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export * from "./alert.js";
+export * from "./benckmark.js";
+export * from "./check.js";
+export * from "./options.js";
+export * from "./rpc.js";
+
+export * from "echarts/dist/echarts.esm.min.mjs"
+export * from "ethers"
+export * from "axios"
