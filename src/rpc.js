2934babb97e758033b2b994007b1a8f3847335e5
imelon279 <imelon279@naver.com>
2024-11-23 15:53:16 +0900

    re"

--- src/rpc.js
+++ src/rpc.js
@@ -20,3 +20,34 @@ const headers = {
       console.error(error);
     }
   };
+  
+  export const arb_getBlockNumber = async (url, params) => {
+    try {
+      const {data} = await axios.post(
+        url,
+        { method: "eth_getBlockNumber", params,...postData },
+        { headers }
+      );
+      return data.result;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  export const eth_chainId = async (url) => {
+    try {
+      const { data } = await axios.post(
+        url,
+        {
+          jsonrpc: "2.0",
+          method: "eth_chainId",
+          params: [],
+          id: 1,
+        },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      return data.result;
+    } catch (error) {
+      console.error(error);
+    }
+  };
